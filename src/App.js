e6bd90d7a878ffee417f05463c7d8adb16122bac
Yash Lad <yashlad727@gmail.com>
2023-07-19 12:49:56 +0530

    adding header details and food img

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Fragment } from 'react';
+import Header from './components/Layout/Header';
 
 function App() {
   return (
-    <div></div>
+    <Fragment>
+      <Header />
+    </Fragment>
   );
 }
 
