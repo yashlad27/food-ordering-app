cc4bc2fd6061452de773863ada90ae1dfa8db63e
Yash Lad <yashlad727@gmail.com>
2023-07-19 12:50:13 +0530

    header details added and background img

---
+++ src/components/Layout/Header.jsx
@@ -0,0 +1,17 @@
+import { Fragment } from "react";
+import mealsImage from '../../assets/meals.jpg';
+import classes from './Header.module.css';
+
+const Header = props => {
+    return <Fragment>
+        <header className={classes.header}>
+            <h1>React Meals</h1>
+            <button>Cart</button>
+        </header>
+        <div className={classes['main-image']}>
+            <img src={mealsImage} alt="a table full of delicious food"/>
+        </div>
+    </Fragment>
+};
+
+export default Header;
